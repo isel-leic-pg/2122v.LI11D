4ed80bf5c20c496a2ed79fd3724e60b09d8a4066
Pedro Pereira <palex@cc.isel.ipl.pt>
2022-03-24 15:52:15 +0000

    Aula 24 Março
    Funções

---
+++ Aula5/Sort.kt
@@ -0,0 +1,22 @@
+fun main() {
+   val a = readInt(base=10)
+   val b = readInt(base=2, quest="Valor 2")
+   val c = readInt("Valor 3", base=10)
+
+   val txt =
+     if (a<=b)
+	   if (b<=c) "$a $b $c"
+	   else
+	     if (c<=a) "$c $a $b"
+	     else "$a $c $b"
+	 else
+	   if(a<=c) "$b $a $c"
+	   else 
+	     if (b<=c) "$b $c $a"
+	     else "$c $b $a" 
+   println(txt)
+}
+
+
+
+
